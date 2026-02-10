b3519de705ac48240e6d6a52017a9069ce981e5b
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-10 21:43:40 +0300

    add change password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,19 @@ class ApplicationController < ActionController::Base
   # this method will run and set the current user in the Current class
   # the first thing that happens when a request comes
 
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  # Changes to the importmap will invalidate the etag for HTML responses
+  stale_when_importmap_changes
+
   def set_current_user
     if session[:user_id]
       Current.user = User.find_by(id: session[:user_id]) # find throws when not found , find_by doesn't throw
     end
   end
 
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-
-  # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
+  def require_user_logged_in!
+    redirect_to sign_in_path, alert: "You must be logged in to change password" if Current.user.nil?
+  end
 end
