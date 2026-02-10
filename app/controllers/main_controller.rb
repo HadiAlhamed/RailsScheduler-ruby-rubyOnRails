3f7f4db6438d18ba6b91b29ad341dd6510d08bb8
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-10 03:59:22 +0300

    format files

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController  < ApplicationController
     def index
         if session[:user_id]
-             @user = User.find_by(id: session[:user_id]) #find throws when not found , find_by doesn't throw
+             @user = User.find_by(id: session[:user_id]) # find throws when not found , find_by doesn't throw
         end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
-#email:string
-#password_digest:string
-#has_secure_password adds some virtual attributes to the model, 
-#password:virtual string
-#password_confirmation:virtual string
+# email:string
+# password_digest:string
+# has_secure_password adds some virtual attributes to the model,
+# password:virtual string
+# password_confirmation:virtual string
 class User < ApplicationRecord
     has_secure_password
-    validates :email, presence: true , format: { with: URI::MailTo::EMAIL_REGEXP , message: "must be a valid email address" }
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 end
