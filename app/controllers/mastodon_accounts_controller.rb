23a2079408feb0328a198b57341ec16cb9221333
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-12 19:51:05 +0300

    user before action fo setting the account

--- app/controllers/mastodon_accounts_controller.rb
+++ app/controllers/mastodon_accounts_controller.rb
@@ -1,12 +1,17 @@
 class MastodonAccountsController < ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_mastodon_account, only: [ :destroy ]
   def index
     @mastodon_accounts = Current.user.mastodon_accounts.all
   end
 
   def destroy
-    @account = Current.user.mastodon_accounts.find(params[:id])
     @account.destroy
     redirect_to mastodon_accounts_path, notice: "Successfully disconnected @#{@account.username}"
+  end
+
+  private
+  def set_mastodon_account
+        @account = Current.user.mastodon_accounts.find(params[:id])
   end
 end
