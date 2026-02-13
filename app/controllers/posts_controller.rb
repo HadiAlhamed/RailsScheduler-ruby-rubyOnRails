d971be54e9f4de2c483a39d15d1750a04621219d
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-13 03:02:57 +0300

    edit and destory scheduled posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
   def index
     @posts = Current.user.posts
   end
@@ -24,13 +25,23 @@ class PostsController < ApplicationController
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "Post was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
   #
   def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "Post was successfully unscheduled."
   end
 
   private
   def post_params
     params.require(:post).permit(:mastodon_account_id, :body, :publish_at)
+  end
+  def set_post
+    @post = Current.user.posts.find(params[:id])
   end
 end
